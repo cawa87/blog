5bcc040e0335eebf71c72896e1a5fa81458b8666
Cawa <cawa123@mail.ru>
2013-07-27 01:34:30 +0300

    doctrine config

---
+++ config/DoctrineConfig.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+* @description doctrine Doctrine ORM config
+* @author CawaKharkov
+*/
+
+use Doctrine\ORM\EntityManager,
+    Doctrine\ORM\Configuration,
+    Doctrine\Common\Cache\ArrayCache as Cache,
+    Doctrine\Common\Annotations\AnnotationRegistry;
+
+//configuration
+$config = new Configuration();
+$cache = new Cache();
+$config->setQueryCacheImpl($cache);
+$config->setProxyDir(__DIR__ . '/data/EntityProxy');
+$config->setProxyNamespace('EntityProxy');
+$config->setAutoGenerateProxyClasses(true);
+
+//mapping (example uses annotations, could be any of XML/YAML or plain PHP)
+AnnotationRegistry::registerFile(__DIR__ . '/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php');
+$driver = new \Doctrine\ORM\Mapping\Driver\AnnotationDriver(
+        new \Doctrine\Common\Annotations\AnnotationReader(), array(__DIR__ . '/app/Entity')
+);
+$config->setMetadataDriverImpl($driver);
+$config->setMetadataCacheImpl($cache);
+
+//getting the EntityManager
+$entityManager = EntityManager::create(
+                array(
+            'driver' => 'pdo_mysql',
+            'user' => 'root',
+            'password' => 'cawa123azs',
+            'dbname' => 'blog',
+            'charset' => 'utf8'
+                ), $config
+);
