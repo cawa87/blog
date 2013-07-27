2eae1f5f06879b724ee22169a3679cc88c60d6fa
Cawa <cawa123@mail.ru>
2013-07-27 16:46:39 +0300

    config local

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
@@ -9,16 +10,15 @@ chdir(dirname(__DIR__));
 define('BASE_DIR', dirname(__DIR__));
 
 require '/vendor/autoload.php';
-require 'config/DoctrineConfig.php';
+
+//define environment dev/prod
+if(is_readable('config/appConfig.local.php') && is_readable('config/DoctrineConfig.local.php')){
+    define('ENVIRONMENT', 'dev');
+}else{
+    define('ENVIRONMENT', 'prod');
+}
+
 
 // run the app
-Main\Blog::run();
-//$entityManager->getRepository('Entity\Test');
-$test = new Entity\Test();
-$test->setText('asdasdsad');
-//$entityManager->persist($test);
-//$entityManager->flush();
-
-$first = $entityManager->getRepository('Entity\Test')->find(1);
-var_dump($first->setCreated(date()));
-var_dump($first->getCreated());
+$blog = new Main\Blog();
+$blog->run();
