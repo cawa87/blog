b89352dc5b9608d282a381fd00948b9ed10c15ed
Cawa <cawa123@mail.ru>
2013-08-01 02:53:28 +0300

    .htacess

--- app/Main/Doctrine/EntityManagerObject.php
+++ app/Main/Doctrine/EntityManagerObject.php
@@ -21,43 +21,39 @@ class EntityManagerObject
     public function __construct() 
     {
         $config = ConfigReader::readConfig('doctrine');
-        //configuration
         $doctrineConfig = new Configuration();
+        
         $cache = new Cache();
         $doctrineConfig->setQueryCacheImpl($cache);
+        
         $doctrineConfig->setProxyDir($config['proxyDir']);
         $doctrineConfig->setProxyNamespace('Proxy');
         $doctrineConfig->setAutoGenerateProxyClasses(true);
         $doctrineConfig->setResultCacheImpl($cache);
+        
         //mapping (example uses annotations, could be any of XML/YAML or plain PHP)
         AnnotationRegistry::registerFile(BASE_DIR . '/vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php');
         $driver = new \Doctrine\ORM\Mapping\Driver\AnnotationDriver(
                 new \Doctrine\Common\Annotations\AnnotationReader(), array($config['entityDir'])
         );
+        
         $doctrineConfig->setMetadataDriverImpl($driver);
         $doctrineConfig->setMetadataCacheImpl($cache);
 
-         $em = EntityManager::create(array(
-            'driver' => 'pdo_mysql',
-            'user' => 'root',
-            'password' => 'cawa123azs',
-            'dbname' => 'blog',
-            'charset' => 'utf8')
-                , $doctrineConfig
+        $em = EntityManager::create($config
+                        , $doctrineConfig
         );
-         
         $this->setEm($em);
+    }
+
+    public function setEm(EntityManager $em)
+    {
+        $this->em = $em;
+    }
         
-    }     
-        public function setEm(EntityManager $em)
-        {
-            $this->em = $em;
-        }
-        
-        public function getEm() 
-        {
-            return $this->em;
-        }
-    
+    public function getEm() 
+    {
+    return $this->em;
+    }
     
 }
